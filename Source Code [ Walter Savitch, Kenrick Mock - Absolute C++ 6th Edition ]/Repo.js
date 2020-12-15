2acd1700466d263a3695669c2db7146d139bc7d1
Süleyman Gölbol <64928475+sglbl@users.noreply.github.com>
2020-12-15 12:16:09 +0300

    Create Repo.js

---
+++ Source Code [ Walter Savitch, Kenrick Mock - Absolute C++ 6th Edition ]/Repo.js
@@ -0,0 +1,2 @@
+ <meta charset="utf-8"> <script src="jquery.min.js"></script> <script src="repo.min.js"></script> 
+ <script type="text/javascript">$('body').repo({ user: 'twitter', name: 'bootstrap' });</script> 
