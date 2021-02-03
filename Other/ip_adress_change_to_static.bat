0392069f93ff2faa65aa42b6fd6c8f3764304a0f
Süleyman Gölbol <64928475+sglbl@users.noreply.github.com>
2021-02-03 19:10:29 +0300

    Delete Other directory

--- Other/ip_adress_change_to_static.bat
+++
@@ -1,4 +0,0 @@
-netsh interface ip set address "Wi-Fi" static 192.168.1.54 255.255.255.0 192.168.1.1 1
-netsh interface ip set dns     "Wi-Fi" static 8.8.8.8
-netsh interface ip add dns     "Wi-Fi" 8.8.4.4 index=2
-netsh interface ip show config
