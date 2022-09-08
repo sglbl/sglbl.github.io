425daaf38d0a03800a55e8ec4f71b43a99dab871
sglbl <silo1881@gmail.com>
2022-09-08 13:55:04 +0300

    dynamic1

--- Others/Cpp_Source_Codes/Repo.js
+++
@@ -1,2 +0,0 @@
- <meta charset="utf-8"> <script src="jquery.min.js"></script> <script src="repo.min.js"></script> 
- <script type="text/javascript">$('body').repo({ user: 'sglbl', name: 'bootstrap' });</script> 
